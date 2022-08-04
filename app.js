0cd4177faf7fc46d6c990ff8a414903009b1d384
Bünyamin Elmas <belmas.ben@outlook.com>
2022-08-04 12:02:21 +0300

    Ready for Heroku

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
 const ejs = require('ejs');
-const path = require('path');
-const fs = require('fs');
-const Photo = require('./models/Photo');
+const photoController = require('./controllers/photoControllers')
+const pageController = require('./controllers/pageControllers')
+
 
 const app = express();
 
 // CONNETCT DB
-mongoose.connect('mongodb://127.0.0.1:27017/pcat-test-db', {
+mongoose.connect('mongodb+srv://Diamond:B.elmas123@cluster0.ixpxv.mongodb.net/pcat-db?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+}).then(() =>{
+  console.log("DB CONNECTED!");
+}).catch((err) =>{
+  console.log(err);
+})  
 
 //TEMPLATE ENGİNE
 app.set('view engine', 'ejs');
@@ -22,52 +27,24 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
 //ROUTES
-app.get('/', async (req, res) => {
-  const photos = await Photo.find({}).sort('-dateCreated');
-  res.render('index', {
-    photos,
-  });
-});
+app.get('/', photoController.getAllPhotos );
+app.get('/photos/:id', photoController.getPhoto );
+app.post('/photos', photoController.createPhoto );
+app.put('/photos/:id', photoController.updatePhoto);
+app.delete('/photos/:id', photoController.deletePhoto);
 
-app.get('/photos/:id', async (req, res) => {
-  //console.log(req.params.id);
-  //res.render('about');
-  const photo = await Photo.findById(req.params.id);
-  res.render('photo', {
-    photo,
-  });
-});
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-app.get('/add', (req, res) => {
-  res.render('add');
-});
+app.get('/about', pageController.getAboutPage );
+app.get('/add', pageController.getAddPage);
+app.get('/photos/edit/:id', pageController.getEditPage);
 
-app.post('/photos', async (req, res) => {
-  //Eğer klasör yoksa oluşturacak
-  const uploadDir = 'public/uploads';
-  if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir);
-  }
-  // Yükeldiğimiz dosyayı yakalayıp isteiğimiz bilgileri değişkenleri aktarıyoruz
-  let uploadeImage = req.files.image;
-  let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name;
-
-  // Yakaladığımız dosyayı .mv metodu ile yukarda belirlediğimiz path'a taşıyoruz. Dosya taşıma işlemi sırasında hata olmadı ise req.body ve içerisindeki image'nin dosya yolu ve adıyla beraber database kaydediyoruz
-  uploadeImage.mv(uploadPath, async (err) => {
-    if (err) console.log(err);    // Bu kısımda önemli olan add.ejs'nin içerisine form elemanı olarak encType="multipart/form-data" atribute eklemek
-    await Photo.create({
-      ...req.body,
-      image: '/uploads/' + uploadeImage.name,
-    });
-  });
-  res.redirect('/');
-});
-
-const port = 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı...`);
 });
